4990ad54f8c72e2424f8c9434efb18eb8432fcfd
Eddie Lee <eddieberklee@gmail.com>
2016-02-22 20:51:35 -0800

    Firebase essentials added

---
+++ README.md
@@ -0,0 +1,9 @@
+
+A template Android application for Firebase with a basic authentication screen
+
+To get it up and running
+------------------------
+
+* Need to edit gradle.properties with your own unique firebase url.
+
+
---
+++ app/src/main/java/com/compscieddy/firebaseauthenticationtemplate/TemplateApplication.java
@@ -0,0 +1,21 @@
+package com.compscieddy.firebaseauthenticationtemplate;
+
+import android.app.Application;
+
+import com.firebase.client.Firebase;
+
+/**
+ * Created by elee on 2/22/16.
+ */
+public class TemplateApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    /* Initialize Firebase */
+    Firebase.setAndroidContext(this);
+    /* Enable disk persistence  */
+    Firebase.getDefaultConfig().setPersistenceEnabled(true);
+  }
+}
