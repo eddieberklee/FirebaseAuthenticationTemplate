8c7c9331d86dae0bf72cbfc6bba3f9130b7392c7
Eddie Lee <eddieberklee@gmail.com>
2016-03-01 14:53:33 -0800

    Send directly to main activity if user already logged in

--- app/src/main/java/com/compscieddy/firebaseauthtemplate/BaseActivity.java
+++ app/src/main/java/com/compscieddy/firebaseauthtemplate/BaseActivity.java
@@ -2,15 +2,56 @@ package com.compscieddy.firebaseauthtemplate;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
+import com.compscieddy.eddie_utils.Etils;
+import com.compscieddy.eddie_utils.Lawg;
+import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
 
 /**
  * Created by elee on 2/29/16.
  */
 public class BaseActivity extends AppCompatActivity {
+
+  private static final Lawg lawg = Lawg.newInstance(BaseActivity.class.getSimpleName());
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    Firebase ref = new Firebase(Constants.FIREBASE_URL);
+    AuthData authData = ref.getAuth();
+    if (authData == null) {
+      lawg.d("User not logged in - booting them to the AuthenticationActivity");
+      if (!(this instanceof AuthenticationActivity)) {
+        Intent intent = new Intent(BaseActivity.this, AuthenticationActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+      }
+    } else {
+      String email = (String) authData.getProviderData().get("email");
+      String encodedEmail = Etils.encodeEmail(email);
+      SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(BaseActivity.this);
+      sharedPreferences.edit().putString(Constants.PREF_KEY_EMAIL, encodedEmail).apply();
+      if (this instanceof AuthenticationActivity) { // don't want to be redirecting if we're already on the ClockActivity
+        Intent intent = new Intent(BaseActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+      }
+    }
+  }
+
+  protected String getEncodedEmail() {
+    AuthData authData = new Firebase(Constants.FIREBASE_URL).getAuth();
+    String email = (String) authData.getProviderData().get("email");
+    String encodedEmail = Etils.encodeEmail(email);
+    return encodedEmail;
+  }
 
   protected void logout() {
     new Firebase(Constants.FIREBASE_URL).unauth();
--- app/src/main/java/com/compscieddy/firebaseauthtemplate/Constants.java
+++ app/src/main/java/com/compscieddy/firebaseauthtemplate/Constants.java
@@ -8,4 +8,8 @@ public class Constants {
   public static final String FIREBASE_URL = BuildConfig.UNIQUE_FIREBASE_ROOT_URL;
   public static final String FIREBASE_URL_USERS = BuildConfig.UNIQUE_FIREBASE_ROOT_URL + "/" + "users";
 
+  /** Preference Key Constants */
+
+  public static final String PREF_KEY_EMAIL = "pref_key_email";
+
 }
